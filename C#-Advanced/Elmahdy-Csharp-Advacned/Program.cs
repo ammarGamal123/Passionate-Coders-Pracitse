065c0c136f7c8cd62dd4e30aa7e69711db1f19e4
Ammar Hammad <amargamal832909@gmail.com>
2024-09-19 15:34:08 +0300

    learn about recursion with a good small project

--- C#-Advanced/Elmahdy-Csharp-Advacned/Program.cs
+++ C#-Advanced/Elmahdy-Csharp-Advacned/Program.cs
@@ -1,23 +1,59 @@
-﻿using Elmahdy_Csharp_Advacned.Extension;
+﻿
+
+string dirPath = @"D:\Audio";
+int level = 1;
+PrintFileSystemEnteries(dirPath, level);
+
+static void PrintFileSystemEnteries(string path , int level)
+{
+    foreach(var fileName in Directory.GetFiles(path))
+        Console.WriteLine($"{new string('-' , level)}:" +
+            $"{new FileInfo(fileName).Name}");
+
+    foreach (var dirName in Directory.GetDirectories(path))
+    {
+        Console.WriteLine($"{new string('-', level)}:" +
+            $"{new DirectoryInfo(dirName).Name}");
+
+        PrintFileSystemEnteries(dirName, level + 1);
+    }
+}
+
+Console.WriteLine($"\n\n\n\nTotal Size = {CalculateDirectoriesSize(dirPath)} bytes");
+
+static long CalculateDirectoriesSize(string path)
+{
+    long totalSize = 0;
+    foreach (var fileName in Directory.GetFiles(path))
+    {
+        totalSize += new FileInfo(fileName).Length;
+    }
+
+    foreach (var dirName in Directory.GetDirectories(path))
+    {
+        totalSize += CalculateDirectoriesSize(dirName);
+    }
+
+    return totalSize;
+}
 
 
-string str = "Ammar Hammad is the best programmer";
-
-Console.WriteLine(str.RemoveWhiteSpaces().Reverse() + '\n');
-
-
-int percentage = -10;
-
-if (percentage.NumberIsBetweenRange(0 , 100))
-    Console.WriteLine("Percentage is valid");
-else
-    Console.WriteLine("Invalid Percentage");
 
 
 
+#region Factorial
+int value = 5;
+Console.WriteLine($"\n\n\nFactorial of {value} is {CalculateFactorial(value)}");
 
 
+static int CalculateFactorial(int number)
+{
+    if (number <= 1)
+        return number;
 
+    return number * CalculateFactorial(number - 1);
+}
+#endregion
 
 Console.ReadKey();
 
